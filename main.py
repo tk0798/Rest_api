ac42b1689397c84e488da2a4fd8ee23b354caa1b
tk0798 <kaantutuk1@hotmail.com>
2021-10-26 11:12:09 +0300

    third

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify
-from flask import make_response
 from flask import request
-import json
 products = [
     {
         'id': 1,
         'model_id': 1,
         'model_adi': '04.00.006',
-        'longitude': '35.421585',
-        'latitude': '38.543869',
+        'longitude': 35421585,
+        'latitude': 38543869,
         'toplam_adim':55,
         'kalori':19,
         'oksijen': 96,
@@ -23,8 +21,8 @@ products = [
         'id': 2,
         'model_id': 1,
         'model_adi': '04.00.006',
-        'longitude': '35.429371',
-        'latitude': '38.543868',
+        'longitude': 35429371,
+        'latitude': 38543868,
         'toplam_adim':75,
         'kalori':20,
         'oksijen': 96,
@@ -39,8 +37,8 @@ products = [
         'id': 3,
         'model_id': 2,
         'model_adi': '04.00.002',
-        'longitude': '35.421581',
-        'latitude': '38.548167',
+        'longitude': 35421581,
+        'latitude': 38548167,
         'toplam_adim':30,
         'kalori':10,
         'oksijen': '94',
@@ -56,21 +54,12 @@ products = [
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/azon/api/products', methods=['GET'])
 def get_products():
     return jsonify({'products': products})
-# import pyodbc
-#
-# db = pyodbc.connect(
-#             Driver='{SQL Server}',
-#             Server='DESKTOP-40GMAHB\MSSSQLSERVER',
-#             DATABASE='saat',
-#             Trusted_Connection='True'
-#         )
-# imlec = db.cursor()
 
 
-@app.route('/<int:product_id>', methods=['GET'])
+@app.route('/azon/api/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
     product = [product for product in products if product['id']==product_id]
 
@@ -87,21 +76,12 @@ def get_product(product_id):
         print("product seri_no :", product[i]['seri_no'])
         print("product yazilim_versiyon :", product[i]['yazilim_versiyon'])
         print("product tarih :", product[i]['tarih'])
-    #     try:
-    #         imlec.execute('INSERT INTO saat_veri (kullanici_id,model_id,longitude,latitude,toplam_adim,kalori,oksijen,nabiz,seri_no,yazilim_versiyon,tarih) values (?,?,?,?,?,?,?,?,?,?,?)',
-    #                       product[i]['kullanici_id'], product[i]['model_id'],product[i]['longitude'],product[i]['latitude'],product[i]['toplam_adim'],product[i]['kalori'],product[i]['oksijen'],product[i]['nabiz'],product[i]['seri_no'],product[i]['yazilim_versiyon'],product[i]['tarih'])
-    #         db.commit()
-    #         print("1 kayıt eklendi")
-    #     except Exception as e:
-    #         print(e)
-    #         print("olmadı 18")
-    #
+
     if len(product) == 0:
         return jsonify({'product': 'Not found'}), 404
     return jsonify({'product': product})
 
-
-@app.route('/', methods=['POST'])
+@app.route('/azon/api/products/', methods=['POST'])
 def create_product():
     newProduct = {
         'id': request.json['id'],
@@ -122,20 +102,13 @@ def create_product():
     return jsonify({'product': newProduct}), 201
 
 
-@app.route('/<int:product_id>', methods=['DELETE'])
+@app.route('/azon/api/products/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
     product = [product for product in products if product['id'] == product_id]
     if len(product) == 0:
         return jsonify({'product': 'Not found'}), 404
     products.remove(product[0])
     return jsonify({'result': True})
-
-
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(
-#         jsonify({'HTTP 404 Error': 'The content you looks for does not exist. Please check your request.'}), 404)
-
 
 if __name__ == '__main__':
     app.run(debug=True)  # !flask/bin/python
